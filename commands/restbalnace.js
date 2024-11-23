9e8efd8537ba98b5ec9266460b09e3cae71eba8e
baonq0507 <nquangbao0507@gmail.com>
2024-11-23 08:03:14 +0700

    develop

--- commands/restbalnace.js
+++ commands/restbalnace.js
@@ -1,17 +1,12 @@
 const User = require('../models/user');
-const { PASSWORD } = process.env;
+const { APP_NAME, RESTBALANCECOMMAND } = process.env;
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('restbalance')
+        .setName(RESTBALANCECOMMAND)
         .setDescription('Khôi phục số tiền ban đầu'),
     async execute(message, args) {
         console.log(message.author.id);
-        const password = args[0];
-        if (password !== PASSWORD) {
-            await message.reply('Mật khẩu không đúng!');
-            return;
-        }
         const users = await User.find();
         for (const user of users) {
             user.balance = 100000;
@@ -22,6 +17,6 @@ module.exports = {
             .setDescription('Khôi phục số tiền ban đầu thành công!')
             .setColor('Green')
             .setThumbnail('https://i.ibb.co/PzpqhNg/464364317-1044910207314456-4180777111429000799-n.jpg')
-        await message.reply({ embeds: [embed] });
+        await message.reply({ content: `${APP_NAME} đã khôi phục số tiền ban đầu thành công! ${message.author.id}`, embeds: [embed] });
     }
 }
