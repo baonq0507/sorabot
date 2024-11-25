f89af3d17ca7c85501f078a7aba6c88e04346531
baonq0507 <nquangbao0507@gmail.com>
2024-11-25 19:59:05 +0700

    develop

--- commands/trutien.js
+++ commands/trutien.js
@@ -5,16 +5,9 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('trutien')
         .setDescription('Trừ tiền')
-        .addUserOption(option => option.setName('user').setDescription('Người nhận').setRequired(true))
-        .addIntegerOption(option => option.setName('amount').setDescription('Số tiền').setRequired(true)),
+        .addUserOption(option => option.setName('user').setDescription('Người nhận').setRequired(true)),
     async execute(message, args) {
         const user = message.mentions.users.first();
-        const amount = parseInt(args[1]);
-
-        if (isNaN(amount) || amount <= 0) {
-            await message.reply('Số tiền không hợp lệ! Vui lòng nhập số tiền lớn hơn 0! 💰 💰 💰');
-            return;
-        }
 
         const userData = await User.findOne({ discordId: user.id });
         if (!userData) {
