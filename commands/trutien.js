a873f1d4e8c42db771cfd8fb3a2d8b71778d0e2a
baonq0507 <nquangbao0507@gmail.com>
2024-11-25 19:57:24 +0700

    develop

---
+++ commands/trutien.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { formatNumber } = require('../common');
+const User = require('../models/user');
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('trutien')
+        .setDescription('Trừ tiền')
+        .addUserOption(option => option.setName('user').setDescription('Người nhận').setRequired(true))
+        .addIntegerOption(option => option.setName('amount').setDescription('Số tiền').setRequired(true)),
+    async execute(message, args) {
+        const user = message.mentions.users.first();
+        const amount = parseInt(args[1]);
+
+        if (isNaN(amount) || amount <= 0) {
+            await message.reply('Số tiền không hợp lệ! Vui lòng nhập số tiền lớn hơn 0! 💰 💰 💰');
+            return;
+        }
+
+        await User.updateOne({ discordId: user.id }, { $inc: { balance: -amount } });
+        await message.reply(`${user.displayName} đã trừ ${formatNumber(amount)} 💰`);
+    }
+
+
+};
